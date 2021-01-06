b6bf209231dc03c72eaa9db67440e50897dcdaac
Paige Watson <paige.watson@outlook.com>
2021-01-05 16:18:32 -0800

    Practice

--- CSharp/FakeItTillYouMakeIt/FizzBuzzTest.cs
+++ CSharp/FakeItTillYouMakeIt/FizzBuzzTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using ApprovalTests.Utilities;
 using ApprovalUtilities.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -11,10 +12,10 @@ namespace FizzBuzzFake
         [TestMethod]
         public void TestFizzBuzz()
         {
-            var java = PathUtilities.LocateFileFromEnviormentPath("javaw.exe");
+            var java = PathUtilities.LocateFileFromEnvironmentPath("javaw.exe");
             var counterDisplayJar = PathUtilities.GetAdjacentFile("CounterDisplay.jar");
-            TestCounter.ResetAndLaunch(java.FirstOrDefault(), counterDisplayJar);// Comment out after 1st run
-            TestCounter.Track(Check20);
+            // TestCounter.ResetAndLaunch(java.FirstOrDefault(), counterDisplayJar);// Comment out after 1st run
+           // TestCounter.Track(Check20);
         }
 
         private static void Check20()
@@ -26,10 +27,45 @@ namespace FizzBuzzFake
 
     public class FizzBuzz
     {
+        /// <summary>
+        /// 1) separate
+        /// 2) encapsulate
+        /// 3) calculate
+        /// 4) automate
+        /// 5) clean
+        /// </summary>
         public static string Print(int top)
         {
-            string expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
-            return expected;
+            var comma = ",";
+            var parts = new List<string>();
+            var fizz = "Fizz";
+            var buzz = "Buzz";
+            var fizzbuzz = fizz + buzz;
+
+            for (int i = 1; i <= top; i++)
+            {
+                if (i == 15)
+                {
+                    parts.Add(fizzbuzz);
+                    continue;
+                }
+
+                if (i % 5 == 0)
+                {
+                    parts.Add(buzz);
+                    continue;
+                }
+
+                if (i % 3 == 0)
+                {
+                    parts.Add(fizz);
+                    continue;
+                }
+
+                parts.Add(i.ToString());
+            }
+
+            return string.Join(comma, parts);
         }
     }
 }
