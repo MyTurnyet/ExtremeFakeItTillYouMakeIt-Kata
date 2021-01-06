657fd13050cf4f711c92c79f7082dacb4fb53718
Paige Watson <paige.watson@outlook.com>
2021-01-05 16:19:32 -0800

    updated PathUtilities function call

--- CSharp/FakeItTillYouMakeIt/FizzBuzzTest.cs
+++ CSharp/FakeItTillYouMakeIt/FizzBuzzTest.cs
@@ -12,7 +12,7 @@ namespace FizzBuzzFake
         [TestMethod]
         public void TestFizzBuzz()
         {
-            var java = PathUtilities.LocateFileFromEnvironmentPath("javaw.exe");
+            var java = PathUtilities.LocateFileFromEnviormentPath("javaw.exe");
             var counterDisplayJar = PathUtilities.GetAdjacentFile("CounterDisplay.jar");
             // TestCounter.ResetAndLaunch(java.FirstOrDefault(), counterDisplayJar);// Comment out after 1st run
            // TestCounter.Track(Check20);
